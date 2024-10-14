aa4e6603788fa4ba08c9e5c344b2abeb1ce485d3
Nilesh Burla <141109890+Nilesh-Burla1106@users.noreply.github.com>
2024-10-14 16:54:13 +0530

    Update completeTodo.js

--- todo-cli/completeTodo.js
+++ todo-cli/completeTodo.js
@@ -1,6 +1,7 @@
 // completeTodo.js
 var argv = require("minimist")(process.argv.slice(2));
 const db = require("./models/index");
+
 const markAsComplete = async (id) => {
   try {
     await db.Todo.markAsComplete(id);
@@ -10,11 +11,13 @@ const markAsComplete = async (id) => {
 };
 
 (async () => {
-  const { id } = argv;
-  if (!id) {
+  let { id } = argv;
+  if (id === undefined) {
     throw new Error("Need to pass an id");
   }
-  if (!Number.isInteger(id)) {
+  id = parseInt(id, 10);
+
+  if (isNaN(id)) {
     throw new Error("The id needs to be an integer");
   }
   await markAsComplete(id);
