7f7187a9f1fdec67e7ef4586ad681697b74c36af
Nilesh Burla <141109890+Nilesh-Burla1106@users.noreply.github.com>
2024-10-14 20:04:53 +0530

    Update addTodo.js

--- todo-cli/addTodo.js
+++ todo-cli/addTodo.js
@@ -1,11 +1,10 @@
 // addTodo.js
-const argv = require('minimist')(process.argv.slice(2));
+var argv = require("minimist")(process.argv.slice(2));
 const db = require("./models/index");
 
 const createTodo = async (params) => {
   try {
     await db.Todo.addTask(params);
-    console.log(`Added Todo: ${params.title}`);
   } catch (error) {
     console.error(error);
   }
@@ -19,11 +18,12 @@ const getJSDate = (days) => {
   const oneDay = 60 * 60 * 24 * 1000;
   return new Date(today.getTime() + days * oneDay);
 };
-
 (async () => {
   const { title, dueInDays } = argv;
   if (!title || dueInDays === undefined) {
-    throw new Error("title and dueInDays are required. \nSample command: node addTodo.js --title=\"Buy milk\" --dueInDays=-2");
+    throw new Error(
+      'title and dueInDays are required. \nSample command: node addTodo.js --title="Buy milk" --dueInDays=-2 '
+    );
   }
   await createTodo({ title, dueDate: getJSDate(dueInDays), completed: false });
   await db.Todo.showList();
